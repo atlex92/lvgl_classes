0283270046e131e30a066106a4ac2bc2d71264ab
vagrant <vagrant@vbuntu>
2021-09-17 11:40:04 +0000

    semaphor added for toast class

--- LVGLToaster.cpp
+++ LVGLToaster.cpp
@@ -1,4 +1,5 @@
 #include "LVGLToaster.hpp"
+#include "freertos/semphr.h"
 
 #define DEFALT_TOAST_TIME_MS        3000UL
 #define DEFALT_TOAST_ANIMATION_TIME 500UL
@@ -9,33 +10,47 @@ static void changeOpacityCb(lv_obj_t* obj, uint16_t val) {
     lv_obj_set_style_local_opa_scale(obj, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, val);
 }
 
-void LVGLToaster::animationReadyCb(lv_anim_t* old_anim) {
+void LVGLToaster::toastAppearedCallback(lv_anim_t* old_anim) {
 
-    LVGLToaster* toaster = static_cast<LVGLToaster*>(old_anim->var);
-    LVGL_DBG_PRINT("animationReadyCb");
-    lv_anim_set_ready_cb(old_anim, NULL);
+    LVGLToaster* toaster = static_cast<LVGLToaster*>(old_anim->user_data);
+    LVGL_DBG_PRINT("toastAppearedCallback");
+
+    lv_anim_set_ready_cb(old_anim, toastReadyCallback);
 
     lv_anim_set_exec_cb(old_anim, (lv_anim_exec_xcb_t)changeOpacityCb);
 
-    lv_anim_set_delay(old_anim, 3000);
+    lv_anim_set_delay(old_anim, toaster->_toastTimeMs);
     lv_anim_set_values(old_anim, LV_OPA_100, LV_OPA_0);
     lv_anim_start(old_anim);
 }
 
-LVGLToaster::LVGLToaster(LVGLBase* const parent)
+void LVGLToaster::toastReadyCallback(lv_anim_t* old_anim) {
+    LVGLToaster* toaster = static_cast<LVGLToaster*>(old_anim->user_data);
+    LVGL_DBG_PRINT("toastReadyCallback");
+    lv_anim_set_ready_cb(old_anim, NULL);
+
+    if (toaster->_tostReadymphr) {
+        xSemaphoreGive(toaster->_tostReadymphr);
+    }
+}
+
+LVGLToaster::LVGLToaster(SemaphoreHandle_t smphr, LVGLBase* const parent)
     :   LVGLLabel("", parent),
-        _toastTimeMs{DEFALT_TOAST_TIME_MS} {
-    
+        _toastTimeMs{DEFALT_TOAST_TIME_MS},
+        _tostReadymphr{smphr} {
     
     setAlignMode(LV_LABEL_ALIGN_CENTER);
     setAutoRealign(true);
+    setLabelLongMode(LV_LABEL_LONG_BREAK);
     applyTheme();
+    setSize(200, 50);
     hide();
 }
 
-LVGLToaster::LVGLToaster(lv_obj_t* const parent)
+LVGLToaster::LVGLToaster(SemaphoreHandle_t smphr, lv_obj_t* const parent)
     :   LVGLLabel("", parent),
-        _toastTimeMs{DEFALT_TOAST_TIME_MS} {
+        _toastTimeMs{DEFALT_TOAST_TIME_MS},
+        _tostReadymphr{smphr} {
 
     setAlignMode(LV_LABEL_ALIGN_CENTER);
     applyTheme();
@@ -48,14 +63,15 @@ void LVGLToaster::setToast(const std::string& txt) {
     show();  
 
     lv_anim_init(&_anim);
-    lv_anim_set_var(&_anim, this->_obj);
+    lv_anim_set_var(&_anim, _obj);
+    _anim.user_data = this;
 
     lv_anim_set_exec_cb(&_anim, (lv_anim_exec_xcb_t)changeOpacityCb);
     lv_anim_set_repeat_count(&_anim, 1);
 
     lv_anim_set_values(&_anim, LV_OPA_0, LV_OPA_100);
     lv_anim_set_time(&_anim, DEFALT_TOAST_ANIMATION_TIME);
-    lv_anim_set_ready_cb(&_anim, animationReadyCb);
+    lv_anim_set_ready_cb(&_anim, toastAppearedCallback);
     lv_anim_start(&_anim);
 }
 
--- LVGLToaster.hpp
+++ LVGLToaster.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "LVGLLabel.hpp"
+#include "FreeRTOS.h"
+#include "freertos/semphr.h"
 
 class LVGLToaster : public LVGLLabel {
 
     public:
         eLvglType type() const override {return eLvglType::LVGL_CLASS_TOASTER;}
-        explicit LVGLToaster(LVGLBase* const parent = NULL);
-        explicit LVGLToaster(lv_obj_t* const parent);
+        explicit LVGLToaster(SemaphoreHandle_t smphr, LVGLBase* const parent = NULL);
+        explicit LVGLToaster(SemaphoreHandle_t smphr, lv_obj_t* const parent);
         void setToast(const std::string& txt);
         void setTime(const size_t timeMs);
     private:
-        static void animationReadyCb(lv_anim_t* old_anim);
+        static void toastAppearedCallback(lv_anim_t* old_anim);
+        static void toastReadyCallback(lv_anim_t* old_anim);
         size_t _toastTimeMs;
         lv_anim_t _anim;
+        SemaphoreHandle_t _tostReadymphr = NULL;
 };
