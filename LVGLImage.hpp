e33034d2ac1cfd36bbf0511bed28f1f605d08dd8
AlexeyYukhin <vagrant@vbuntu>
2021-11-26 10:48:46 +0000

    Image class update

--- LVGLImage.hpp
+++ LVGLImage.hpp
@@ -4,9 +4,8 @@
 class LVGLImage : public LVGLBase {
 
     public:
-        eLvglType type() const override {return eLvglType::LVGL_CLASS_BATTERY_MAGE;}
+        eLvglType type() const override {return eLvglType::LVGL_CLASS_IMAGE;}
         explicit LVGLImage(const lv_img_dsc_t* img, LVGLBase* const parent = NULL);
-        void zoomOut(const size_t percent);
-    private:
-        const lv_img_dsc_t* _img;
+        void updateImage(const lv_img_dsc_t* img);
+        // void zoomOut(const size_t percent);
 };
